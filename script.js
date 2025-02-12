8566ec217e5cd24f83cf147425b9ae75e033c253
AbelSmn <abelsimonpc@gmail.com>
2025-02-12 11:30:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-document.querySelectorAll('.tile').forEach(tile => {
-  tile.addEventListener('click', () => {
-    alert(`${tile.querySelector('.tile-content').textContent} clicked!`);
-  });
-});
+let emoticon = document.getElementById('emoticon');
+
+function changeEmotion(emotion) {
+  switch (emotion) {
+    case 'happy':
+      emoticon.textContent = ':-)';
+      break;
+    case 'sad':
+      emoticon.textContent = ':-(';
+      break;
+    case 'bored':
+      emoticon.textContent = ':-/';
+      break;
+    case 'hungry':
+      emoticon.textContent = ':-o';
+      break;
+  }
+}
+
+function feed() {
+  changeEmotion('happy');
+  alert('You fed your pet!');
+}
+
+function giveWater() {
+  changeEmotion('happy');
+  alert('You gave water to your pet!');
+}
+
+function giveToy() {
+  changeEmotion('bored');
+  alert('You gave a toy to your pet!');
+}
